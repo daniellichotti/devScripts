07f75ad8b0222672e30b25cdb23427b389ba0dd5
daniellichotti <danielclichotti@gmail.com>
2024-01-05 18:12:12 -0300

    enviando o script de criacao react-vite

---
+++ createVite.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+cd ~/dev
+read -p "Digite o nome do projeto: " name_project &&
+pnpm create vite $name_project --template react-ts &&
+cd "$name_project" &&
+echo 'export function App() {
+    return <h1>hello world</h1>;
+}' > src/App.tsx &&
+echo 'import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+' > src/main.tsx &&
+
+
+rm -rf README.md .eslintrc.cjs src/App.css src/index.css public/vite.svg src/assets &&
+
+sed -i '/"eslint/d' package.json &&
+sed -i '/<link rel="icon"/d' index.html &&
+sed -i 's|<title>Vite + React + TS</title>|<title>'$name_project'</title>|' index.html
+
+
+
+echo "Digite a URL do repositório Git, se existir; caso não exista, deixe vazia:"
+read input
+
+if [ -z "$input" ]; then
+    echo "Projeto nao adicionado ao github."
+else
+    echo name_project >> README.md &&
+    git init &&
+    git add . &&
+    git commit -m "first commit" &&
+    git branch -M main &&
+    git remote add origin "$input" &&
+    git push -u origin main
+fi
+
+pnpm i &&
+code .
+
+
