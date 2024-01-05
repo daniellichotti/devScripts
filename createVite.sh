156e32ba0c91355ce86afbc80157aff01d472d5a
daniellichotti <danielclichotti@gmail.com>
2024-01-05 18:25:20 -0300

    alterando o arquivo

--- createVite.sh
+++ createVite.sh
@@ -1,12 +1,4 @@
 #!/bin/zsh
 
-cd ~/dev
-
-read -p "Digite o nome do projeto: " name_project &&
-pnpm create vite $name_project --template react-ts &&
-cd "$name_project" &&
-
 echo 'Hello World' > hello.txt
-
-# Restante do seu script
 
